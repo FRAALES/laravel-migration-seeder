28b6a38cf80aff77ab96e013ca2f70c9b4ec457e
FRAALES <alessandrofranco1992@gmail.com>
2023-07-13 19:15:46 +0200

    factory fattoo

---
+++ database/factories/RailwayFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Railway>
+ */
+class RailwayFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'azienda' => fake() -> words(),
+            'stazione_di_partenza' => fake() -> city(),
+            'stazione_di_arrivo' => fake() -> city(),
+            'orario_di_partenza' => fake() -> dateTime(),
+            'orario_di_arrivo' => fake() -> dateTime(),
+            'codice_treno' => fake() -> bothify('??-#####'),
+            'numero_carrozze' => fake() -> randomDigit(),
+            'in_orario' => fake() -> boolean(),
+            'cancellato' => fake() -> boolean(),
+
+        ];
+    }
+}
+
+
+// $table->string('azienda'); 
+// $table->string('stazione_di_partenza');
+// $table->string('stazione_di_arrivo');
+// $table->time('orario_di_partenza');
+// $table->time('orario_di_arrivo');
+// $table->string('codice_treno')->unique();
+// $table->integer('numero_carrozze');
+// $table->boolean('in_orario');
+// $table->boolean('cancellato');
